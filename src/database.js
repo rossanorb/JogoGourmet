a147f23b1d3cee1a6b2bc257c9a719bc3ec3fe54
Rossano Bavaresco <peo_rbavaresco@uolinc.com>
2020-11-28 12:13:33 -0300

    alteração em tabela e inserção de registros

--- src/database.js
+++ src/database.js
@@ -6,24 +6,27 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
       console.error(err.message)
       throw err
-    }else{
+    } else {
         console.log('Connected to the SQlite database.')
         db.run(`CREATE TABLE pratos (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             prato text,
             n INTEGER,
-            s INTEGER,
-            ref INTEGER
+            s INTEGER,            
+            ref INTEGER,
+            optref text
             )`,(err) => {
-        if (err) {
-            console.error(err.message)
-        } else {
-            let insert = 'INSERT INTO pratos (prato, n, s, ref) VALUES (?,?,?,?)'
-            db.run(insert, ["massa", 2, 3, null])
-            db.run(insert, ["bolo de chocolate", null, null, 1])
-            db.run(insert, ["lasanha", null, null, 1])
-        }
-    })  
+                if (err) {
+                    console.error(err.message)
+                } else {
+                    let insert = 'INSERT INTO pratos (prato, n, s, ref, optref) VALUES (?, ?, ?, ?, ?)'
+                    db.run(insert, ["massa", 2, 3, null, ''],function(err){
+                        db.run(insert, ["bolo de chocolate", null, null, 1, 'n'], function(err){
+                            db.run(insert, ["lasanha", null, null, 1, 's'])
+                        })
+                    })
+                }
+            })  
     }
 })
 
