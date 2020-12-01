4f7a6e2b31a91b6f6e1493d1499b39da17b7e4d9
Rossano Bavaresco <peo_rbavaresco@uolinc.com>
2020-11-30 22:48:19 -0300

    removido classe desnecessária

--- src/routes/ciclo.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/index-controller');
-
-
-router.get('/', controller.next);
-
-module.exports = router;
